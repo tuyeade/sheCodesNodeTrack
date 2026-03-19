348d7a2819bcf2fd28276b90daa2abbc8081b7f8
unknown <trufatadmasu@gmail.com>
2026-03-18 23:19:04 -0700

    update my code

--- app.js
+++ app.js
@@ -1,39 +1,12 @@
-const readline = require("readline");
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question("Enter first number: ", (num1) => {
-  rl.question("Enter second number: ", (num2) => {
-    rl.question("Choose operation (+, -, *, /): ", (op) => {
-
-      const a = Number(num1);
-      const b = Number(num2);
-      let result;
-
-      if (op === "+") {
-        result = a + b;
-      } else if (op === "-") {
-        result = a - b;
-      } else if (op === "*") {
-        result = a * b;
-      } else if (op === "/") {
-        if (b === 0) {
-          console.log("Error: Cannot divide by zero");
-          rl.close();
-          return;
-        }
-        result = a / b;
-      } else {
-        console.log("Invalid operation");
-        rl.close();
-        return;
-      }
-
-      console.log("Result:", result);
-      rl.close();
-    });
-  });
-});
+function calculator(a, b) {
+  console.log("number: ", a, b);
+  console.log("add: ", a + b);
+  console.log("subtract: ", a - b);
+  console.log("multiplay: ", a * b);
+  if (b == 0) {
+    console.log("canot divide by zero");
+  } else {
+    console.log("divide: ", a / b);
+  }
+}
+calculator(8, 0);
--- arrloop.js
+++ arrloop.js
@@ -1,1 +1,23 @@
-23wsx
+for (let i = 0; i < 5; i++) {
+  console.log(+i);
+}
+let fruits = ["apple", "banana", "orange"];
+for (let i = 0; i < fruits.length; i++) {
+  console.log(fruits[i]);
+}
+
+for (let fruit of fruits) {
+  console.log(fruit);
+}
+
+let count = 0;
+while (count < 3) {
+  console.log("Count: " + count);
+  count++;
+}
+
+let num = 5;
+do {
+  console.log("Number: " + num);
+  num++;
+} while (num < 3);
